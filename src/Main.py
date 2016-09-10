af91429723f22bb59c7d4474edc6dccba278276a
jdebr <jd.debr@gmail.com>
2016-09-10 12:24:27 -0600

    Some fixes to problem generator

--- src/Main.py
+++ src/Main.py
@@ -76,21 +76,26 @@ def draw_graph():
     and such that it will not cross any other edge'''
     # List of node IDs to check for possible edges
     available_nodes = list(coords.keys())
-    
-    #print(available_nodes)
+
+    # Run until no connections available
     while available_nodes:
+        # Choose random point from available nodes
         selected_point = random.choice(available_nodes)
+        # Boolean to loop until a line is added
         lineAdded = False
-        
         while not lineAdded:
+            # If there's no nodes left to check for selected node, remove from 
+            # available nodes and select a new node
             if not distance[selected_point] :
                 available_nodes.remove(selected_point)
                 break
+            # Get closest point to our chosen point and remove it from dictionary
+            # to indicate we have tried to add an edge to it
             else : 
                 nearest_point = distance[selected_point][0][0]
                 distance[selected_point].pop(0)
                 
-                # if no edge already exists
+                # if no edge already exists between these points, calculate line equation
                 if nearest_point not in graph[selected_point]:
                     # set coordinates for calculation
                     pt1 = coords[selected_point]
@@ -100,35 +105,41 @@ def draw_graph():
                     x2 = pt2[0]
                     y2 = pt2[1]
                     
-                    # Check if vertical line
+                    # Check if vertical line, might need to handle differently?
                     if x1 == x2:
                         print("Vertical line!")
                         pass
-                    # Check if any intersections
+                    # Check if this line will intersect any other edges in graph
                     else:
+                        # Calculate values for line Ax + By = C
                         A1 = y2 - y1
                         B1 = x1 - x2
                         C1 = A1 * x1 + B1 * y1
-                        
-                        # No lines on graph so no conflict
+                        # No edges on graph so no conflict, add this as first edge
                         if not lines:
-                            lines.append((A1, B1, C1, x1, x2, y1, y2))
-                        # Check all other lines for intersection
+                            graph[selected_point].append(nearest_point)
+                            lines.append((A1, B1, C1, x1, y1, x2, y2))
+                        # Check all other edges for potential intersection
                         else:
+                            intersection_found = False
+                            
                             for line in lines:
+                                # Retrieve pre-calculated line information
                                 A2 = line[0]
                                 B2 = line[1]
                                 C2 = line[2]
                                 x3 = line[3]
-                                x4 = line[4]
-                                y3 = line[5]
+                                y3 = line[4]
+                                x4 = line[5]
                                 y4 = line[6]
                                 
+                                # Calculate determinant of system
                                 det = (A1 * B2) - (A2 * B1)
                                 if det == 0:
-                                    #lines are parallel
+                                    #lines are parallel, handle differently?
                                     print("Parallel!")
                                     pass
+                                # Calculate point of intersection
                                 else:
                                     x = (B2 * C1 - B1 * C2) / det
                                     y = (A1 * C2 - A2 * C1) / det
@@ -141,11 +152,14 @@ def draw_graph():
                                                 if y >= min(y3, y4) and y <= max(y3, y4):
                                                     # this will intersect so move on
                                                     print("Intersection!")
+                                                    intersection_found = True
                                                     break
-                                    # No intersection so add edge and line
-                                    graph[selected_point].append(nearest_point)
-                                    lines.append((A1, B1, C1, x1, x2, y1, y2))
-                                    lineAdded = True
+                                                
+                            # No intersection so add edge and line
+                            if not intersection_found:
+                                graph[selected_point].append(nearest_point)
+                                lines.append((A1, B1, C1, x1, y1, x2, y2))
+                                lineAdded = True
                     
             
             
